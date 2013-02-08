6f9339fd281ba5268bc7e3b00da6a64280898b23
Matthias Kling <m.kling@sprax.eu>
2013-02-08 22:38:00 +0100

    (de-)linting, reveals 4 test bugs

--- test/referee-combinators-test.js
+++ test/referee-combinators-test.js
@@ -1,4 +1,4 @@
-(function(referee, buster) {
+(function (referee, buster) {
     if (typeof require === "function" && typeof module === "object") {
         referee = require("../lib/referee-combinators");
         buster = require("buster");
@@ -6,65 +6,65 @@
     var combinators = referee.combinators;
     var assert = buster.assert;
     var refute = buster.refute;
-    
+
 
     buster.testCase('partial', {
-	'assert': {
-	    'expected and actual': {
-		"pass":function() {
-		    var actual = combinators.assert.equals(42)
-		    refute.exception(function() {
-			actual(42);
-		    });
-		},
-		"fail": function() {
-		    assert.exception(function() {
-			actual(100);
-		    });
-		}
-	    },
-	    'only actual': {
-		'pass': function() {
-		    var actual = combinators.assert.isTrue();
-		    refute.exception(function() {
-			actual(true);
-		    });
-		},
-		'fail': function() {
-		    assert.exception(function() {
-			actual(false);
-		    });
-		}
-	    }
-	},
-	'refute': {
-	    'expected and actual': {
-		"pass":function() {
-		    var actual = combinators.refute.equals(42)
-		    refute.exception(function() {
-			actual(100);
-		    });
-		},
-		"fail": function() {
-		    assert.exception(function() {
-			actual(42);
-		    });
-		}
-	    },
-	    'only actual': {
-		'pass': function() {
-		    var actual = combinators.refute.isTrue();
-		    refute.exception(function() {
-			actual(false);
-		    });
-		},
-		'fail': function() {
-		    assert.exception(function() {
-			actual(true);
-		    });
-		}
-	    }
-	}
-    })
+        'assert': {
+            'expected and actual': {
+                "pass": function () {
+                    var actual = combinators.assert.equals(42);
+                    refute.exception(function () {
+                        actual(42);
+                    });
+                },
+                "fail": function () {
+                    assert.exception(function () {
+                        actual(100);
+                    });
+                }
+            },
+            'only actual': {
+                'pass': function () {
+                    var actual = combinators.assert.isTrue();
+                    refute.exception(function () {
+                        actual(true);
+                    });
+                },
+                'fail': function () {
+                    assert.exception(function () {
+                        actual(false);
+                    });
+                }
+            }
+        },
+        'refute': {
+            'expected and actual': {
+                "pass": function () {
+                    var actual = combinators.refute.equals(42);
+                    refute.exception(function () {
+                        actual(100);
+                    });
+                },
+                "fail": function () {
+                    assert.exception(function () {
+                        actual(42);
+                    });
+                }
+            },
+            'only actual': {
+                'pass': function () {
+                    var actual = combinators.refute.isTrue();
+                    refute.exception(function () {
+                        actual(false);
+                    });
+                },
+                'fail': function () {
+                    assert.exception(function () {
+                        actual(true);
+                    });
+                }
+            }
+        }
+    });
 
-})(this.referee, this.buster);
+}(this.referee, this.buster));
