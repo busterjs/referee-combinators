f7c805adfdea39823355498f3c93a992218eb73b
Matthias Kling <m.kling@sprax.eu>
2013-02-13 22:59:05 +0100

    refactor test-helper-test: extracted custom assertion `isFunctionOrObject`

--- test/test-helper-test.js
+++ test/test-helper-test.js
@@ -1,10 +1,11 @@
 /*jslint maxlen:100 */
 /*jslint regexp:true*/ // allow . and [^...] in regexps 
-(function (testHelper, util, buster, _) {
+(function (testHelper, util, buster, referee, _) {
     if (typeof require === "function" && typeof module === "object") {
         testHelper = require("./test-helper");
         util = require("../lib/util");
         _ = require("lodash");
+        referee = require("referee");
 
         //// This is a temporary workaround for the development-environment:
         //buster = require("buster-node");
@@ -21,34 +22,38 @@
     var formatArgs = util.formatArgs;
     var makeTests = testHelper.makeTests;
 
+    buster.assertions.add('isFunctionOrObject', {
+        assert: function (actual) {
+            return _.isFunction(actual) || _.isObject(actual);
+        },
+        assertMessage: "${2}Expected ${1}${0} to be either function or object",
+        refuteMessage: "${2}Expected ${1}${0} to be neither function nor object",
+        expectation: "toBeFunctionOrObject",
+        values: function (thing, path, message) {
+            return [thing, path ? path + ": " : "", message ? message + " " : ""];
+        }
+    });
 
     buster.testCase("test-helper", {
+
         "makeTests": {
-            "returns object with some properties": function () {
-                var actual = makeTests('isTrue', [], function () {});
-                assert.isObject(actual);
-                assert.greater(_.keys(actual).length, 0, "# of props");
-            },
-            "returns object with own props either functions or objects of which the same is true":
-                function () {
+
+            "returns object with" : {
+                "some properties": function () {
+                    var actual = makeTests('isTrue', [], function () {});
+                    assert.isObject(actual);
+                    assert.greater(_.keys(actual).length, 0, "# of props");
+                },
+
+                "own props either functions or objects of which the same is true": function () {
                     var actual = makeTests('isTrue', [], function (pass, fail) {});
-                    actual.whuteva = { f: function () {}, two: { three : 3} };
+                    //actual.whuteva = { f: function () {}, two: { three : 3} };
                     function recAsserts(o, path) {
                         _.forOwn(o, function (v, k) {
                             var myPath = path + "['" + k + "']";
-                            if (myPath.length > 100) {
-                                throw new Error(myPath);
-                            }
-                            buster.log(myPath);
-                            if (_.isFunction(v)) {
-                                assert(true);
-                            } else {
-                                assert.isObject(v, myPath
-                                    + " should either be function or object with props"
-                                    + " of which the same is true");
-                                if (_.isObject(v)) { // make sure we really pass only objects
-                                    recAsserts(v, myPath);
-                                }
+                            assert.isFunctionOrObject(v, myPath);
+                            if (_.isObject(v)) { // make sure we really pass only objects
+                                recAsserts(v, myPath);
                             }
                         });
                         return true;
@@ -57,7 +62,8 @@
                     assert.isObject(actual);
                     recAsserts(actual, "makeTests(...)");
                 }
+            }
         }
     });
 
-}(this.testHelper, this.util, this.buster, this.lodash));
+}(this.testHelper, this.util, this.buster, this.referee, this.lodash));
